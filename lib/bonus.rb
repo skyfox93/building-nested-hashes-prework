25fa0308b24d0211c47d0d85f50773511507da0e
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-09 18:44:29 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,8 @@ def bonus
    }
   }
 
-  epic_tragedy[:montague[:hero["status"]]]="dead"
-  epic_tragedy[:capulet[:hero["status"]]]="dead"
+  epic_tragedy[:montague[:hero[status]]]="dead"
+  epic_tragedy[:capulet[:hero[status]]]="dead"
 
   
 
